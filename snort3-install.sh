c1ed427a70426cfa0b956417790d2a01457f89be
Louis Bernardo <louis@louisbernardo.com>
2018-04-22 17:19:00 +0200

    Updated Code
    V1.1 
    Fixes:
    - Streamlined code into purpose sections
    - Updated compile parameters to use multiple cores. 
    - Resolved Hyperspace detection by Snort3 compile
    
    Outstanding:
    - safec still not being detected by snort3 compile. Will address in later revision.

--- snort3-install.sh
+++ snort3-install.sh
@@ -1,64 +1,59 @@
 #!/bin/bash
+################################################# NOTICE ##############################################
+# The following has been optimized for dual core compiling. For make options the additional j3 has been used to assign 2 cores. 
+# If you have more cores adjust this accordingly. This is core count +1 e.g. 4 cores you need j 5 and for 8 cores j 9.
+
+#Apt based dependencies
 apt-get update && apt-get install -y asciidoc autoconf autotools-dev bison build-essential  cmake cpputest dblatex flex g++  libpcre3-dev libdumbnet-dev libhwloc-dev  libhsm-bin libluajit-5.1-dev  liblzma-dev libnetfilter-queue-dev libpcap-dev libssl-dev libsqlite3-dev uuid-dev libtool openssl pkg-config source-highlight w3m zlib1g-dev
+mkdir -p /data/install/
+
+#Downloads
+wget http://www.colm.net/files/colm/colm-0.13.0.5.tar.gz
+wget http://www.colm.net/files/ragel/ragel-6.10.tar.gz
+wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz
+wget https://github.com/intel/hyperscan/archive/v4.7.0.tar.gz -O hyperscan-v4.7.0.tar.gz
+wget https://downloads.sourceforge.net/project/safeclib/libsafec-10052013.tar.gz
+wget https://github.com/google/flatbuffers/archive/master.tar.gz -O flatbuffers-master.tar.gz
+wget https://www.snort.org/downloads/snortplus/daq-2.2.2.tar.gz
+
+#Extract
+tar xvzf colm-0.13.0.5.tar.gz
+tar xzvf ragel-6.10.tar.gz
+tar xvzf boost_1_65_1.tar.gz
+tar xvfz hyperscan-v4.7.0.tar.gz
+tar xvzf libsafec-10052013.tar.gz
+tar xvzf flatbuffers-master.tar.gz
+tar xvzf daq-2.2.2.tar.gz
 
 ## Safec
-## Download and install safec for runtime bounds checks on certain legacy C-library calls (this is optional but recommended):
-mkdir -p /data/install/
-cd /data/install/
-wget https://downloads.sourceforge.net/project/safeclib/libsafec-10052013.tar.gz
-tar xvzf libsafec-10052013.tar.gz
-cd libsafec-10052013
+## safec for runtime bounds checks on certain legacy C-library calls (this is optional but recommended):
+cd /data/install/libsafec-10052013
 ./configure
-make
-make install
+make -j3
+make install -j3
 
-## Ragel
+## Ragel/boost/hyperscan/colm
 ## Snort3 will use Hyperscan for fast pattern matching. Hyperscan requires Ragel and the Boost headers:
 
-##Colm is required for Ragel
-cd /data/install
-wget http://www.colm.net/files/colm/colm-0.13.0.5.tar.gz
-tar xvzf colm-0.13.0.5.tar.gz
-cd colm-0.13.0.5
+##Colm
+cd /data/install/colm-0.13.0.5
 ./autogen
 ./configure
-make
-make install
+make -j3
+make install -j3 
 
 ##Ragel
-cd /data/install
-wget http://www.colm.net/files/ragel/ragel-6.10.tar.gz
-tar xzvf ragel-6.10.tar.gz
-cd ragel-6.10
+cd /data/install/ragel-6.10
 ./configure
-make
-sudo make install
-
-
-#### Ragel 7 needs work done to make it work
-#cd /data/install/
-#wget http://www.colm.net/files/ragel/ragel-7.0.0.10.tar.gz
-#tar xvzf ragel-7.0.0.10.tar.gz
-#cd ragel-7.0.0.10
-#./configure
-#make
-#sudo make install
+make -j3
+make install -j3
 
 ## Hyperscan requires the Boost C++ Libraries
-cd /data/install
-wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz
-tar xvzf boost_1_65_1.tar.gz
-cd boost_1_65_1
-
-
 ## Install Hyperscan 4.7.0 from source, referencing the location of the Boost headers source directory:
-cd /data/install
-wget https://github.com/intel/hyperscan/archive/v4.7.0.tar.gz
-tar xvfz v4.7.0.tar.gz
 cd /data/install/hyperscan-4.7.0
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DBOOST_ROOT=/data/install/boost_1_65_1/ /data/install/hyperscan-4.7.0
-make
-make install
+make -j3
+make install -j3
 
 ## To test Hyperscan use the following
 ## cd /data/install/hyperscan-4.7.0
@@ -66,37 +61,34 @@ make install
 
 
 ## Snort has an optional requirement for flatbuffers, A memory efficient serialization library:
-cd /data/install/
-wget https://github.com/google/flatbuffers/archive/master.tar.gz -O flatbuffers-master.tar.gz
-tar xvzf flatbuffers-master.tar.gz
-mkdir flatbuffers-build
-cd flatbuffers-build
-cmake ../flatbuffers-master
-make
-make install
+mkdir -p /data/install/flatbuffers-build
+cd /data/install/flatbuffers-build
+cmake /data/install/flatbuffers-master
+make -j3
+make install -j3
 
 ## DAQ install
-cd /data/install/
-wget https://www.snort.org/downloads/snortplus/daq-2.2.2.tar.gz
-tar -xvzf daq-2.2.2.tar.gz
-cd daq-2.2.2
+cd /data/install/daq-2.2.2
 ./configure
-make
-make install
-
+make -j3 
+make install -j3
 
 ##Update shared libraries:
 ldconfig
 
 cd /data/install/
 git clone git://github.com/snortadmin/snort3.git
-cd snort3
+cd /data/install/snort3
 ./configure_cmake.sh --enable-large-pcap --enable-shell
 cd build
-make
-make install
+make -j3
+make install -j3
 
-echo 'export PATH=$PATH:/usr/local/snort/bin' > ~/.profile
+echo 'export PATH=$PATH:/usr/local/snort/bin' >> ~/.profile
+echo 'export LUA_PATH=/usr/local/snort/include/snort/lua/\?.lua\;\;' >> ~/.profile
+echo 'export SNORT_LUA_PATH=/usr/local/snort/etc/snort' >> ~/.profile
 . ~/.profile
 
-#Hypescan and safec not being detected by snort3 compile process. Will attempt to resolve later
+
+#safec not being detected by snort3 compile process. Will attempt to resolve later
+#hyperscan not compiling with snort3 resolved.
